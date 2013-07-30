60abd9f055eb7cb0c27b8d6d421c368dc48ccd35
Michiel Missotten <michiel@lil-web.fr>
2013-07-30 11:04:51 +0200

    Suite maj Jekyll, probleme avec les plugins et config.

--- _plugins/excerpt.rb
+++ _plugins/excerpt.rb
@@ -1,13 +1,13 @@
 module Jekyll
 
-  class Excerpt < Liquid::Block
+  class ExcerptTag < Liquid::Block
     def render(context)
       # Get the current post's post object
       id = context["page"]["id"]
       posts = context.registers[:site].posts
       post = posts [posts.index {|post| post.id == id}]
 
-      # Put the block contents into the post's excerpt field,
+      # Put the block contents into the post's excerpt_tag field,
       # and also return those contents
       post.data["excerpt"] = super
     end
@@ -15,4 +15,4 @@ module Jekyll
 
 end
 
-Liquid::Template.register_tag('excerpt', Jekyll::Excerpt)
+Liquid::Template.register_tag('excerpt', Jekyll::ExcerptTag)
